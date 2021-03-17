bf67ebf942b1c38e5f59898b386f82c9479c3262
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-03-17 11:53:01 -0400

    Creates classes for Paintings Container and Painting Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import PaintingsContainer from '../PaintingsContainer'
 
 function App() {
   return (
     <div className="App">
       <h1>Art Explorer</h1>
+      <PaintingsContainer />
     </div>
   );
 }
---
+++ src/components/Paintings.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Paintings extends Component {
+
+    render(){
+
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Paintings 
