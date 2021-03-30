a9a1c1cc7c00612c0e247cef24b0a88e55708f42
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-03-30 15:45:44 -0400

    new branch

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ const About = () => {
        <div className="container">
         <div className="jumbotron">
         <h3>Art Explorer was built using the Harvard Art Museum API</h3><br></br>
+        <h2>Testing</h2>
         <h4> Check out the API documentation <a href={"https://harvardartmuseums.org/collections/api"}> here</a></h4>
         </div>
         <img src="https://bostonglobe-prod.cdn.arcpublishing.com/resizer/J8-MJEVkE3smn0QYowre_TQcrjE=/600x0/arc-anglerfish-arc2-prod-bostonglobe.s3.amazonaws.com/public/ICV4XBS7XUI6JKEJ355RY3TR2E.jpg" alt="museum"/>
